b4e6e746396b346e0bfa09935d2b20b700db5506
Goldy <goldianussm@gmail.com>
2024-03-06 13:23:56 +0700

    feat(): Create new project

---
+++ Flowers-Delivery/Flowers-Delivery/Flowers_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flowers_DeliveryApp.swift
+//  Flowers-Delivery
+//
+//  Created by Goldianus Solangius on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Flowers_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
