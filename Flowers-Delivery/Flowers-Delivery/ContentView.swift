2d9c4e778b04f582c7875fc24a8e258b22f94020
Goldy <goldianussm@gmail.com>
2024-03-06 22:37:24 +0700

    feat(): Adding account creation page to facilitate users in signing up

--- Flowers-Delivery/Flowers-Delivery/ContentView.swift
+++ Flowers-Delivery/Flowers-Delivery/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CreateAccountViewController()
         }
         .padding()
     }
@@ -21,4 +18,6 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
+        .previewDisplayName("Dark Mode")
 }
---
+++ Flowers-Delivery/Flowers-Delivery/View/CreateAccountViewController.swift
@@ -0,0 +1,116 @@
+//
+//  CreateAccountViewController.swift
+//  Flowers-Delivery
+//
+//  Created by Goldianus Solangius on 06/03/24.
+//
+
+import SwiftUI
+
+struct CreateAccountViewController: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var isLoggedin: Bool = false
+    
+    @State private var countryCode: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var isSelected: Bool = false
+    
+    let defaultCountryCode = "+62"
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                
+                VStack(alignment: .center, spacing: 10) {
+                    Text("Enter your phone number").bold()
+                        .font(.title2)
+                    Text("We will send SMS with confirmation code.")
+                }
+                
+                HStack {
+                    Text(defaultCountryCode)
+                        .padding()
+                        .foregroundColor(.gray)
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+                        .background(Color(UIColor.systemGray6))
+                        .disabled(true)
+                    
+                    
+                    TextField("Nomor Telepon", text: $phoneNumber)
+                        .padding()
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+                        .background(Color(UIColor.systemGray6))
+                        .keyboardType(.numberPad)
+                    
+                    Spacer()
+                }
+                
+                HStack {
+                    RadioButton(selected: $isSelected)
+                    Text("Entering the code means that you agree to the Terms of Services and the Personal Data Processing Policy")
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Send")
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                }
+                .disabled(countryCode.isEmpty || phoneNumber.isEmpty)
+                .opacity((countryCode.isEmpty || phoneNumber.isEmpty) ? 0.5 : 1)
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Buat Akun")
+        }
+        .sheet(isPresented: $isLoggedin, content: {
+            Text("Welcome \(username)")
+        })
+    }
+}
+
+#Preview {
+    CreateAccountViewController()
+}
+
+
+
+struct RadioButton: View {
+    @Binding var selected: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.selected.toggle()
+        }) {
+            ZStack {
+                Circle()
+                    .stroke(Color.blue, lineWidth: 2)
+                    .frame(width: 20, height: 20)
+                
+                if selected {
+                    Circle()
+                        .fill(Color.blue)
+                        .frame(width: 10, height: 10)
+                }
+            }
+        }
+    }
+}
+
